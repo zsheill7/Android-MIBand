5affbc5eee49ef4fd3d084273aeafbd2b0ca5baa
zsheill7 <zsheill7@gmail.com>
2016-11-30 11:34:08 -0800

    hello

--- ParseStarterProject/src/main/java/com/parse/starter/Main2Activity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/Main2Activity.java
@@ -86,6 +86,9 @@ public class Main2Activity extends AppCompatActivity {
         } else if (item.getItemId() == R.id.logout) {
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
+        } else if (item.getItemId() == R.id.viewFeed) {
+            Intent intent = new Intent(getApplicationContext(), Feed.class);
+            startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
     }
